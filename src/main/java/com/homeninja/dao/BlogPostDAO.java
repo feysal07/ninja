6a7aaa272b648b51e93092dfc105176c951ad922
evgenyustimenko <evgenyustimenko@f0de6d7c-5c98-4a70-9b99-ebcc080b4637>
2014-10-14 18:44:17 +0000

    Hibernate DAO

--- src/main/java/com/homeninja/dao/BlogPostDAO.java
+++ src/main/java/com/homeninja/dao/BlogPostDAO.java
@@ -1,5 +1,15 @@
 package com.homeninja.dao;
 
+import java.util.List;
+
+import com.homeninja.entities.BlogPost;
+
 public interface BlogPostDAO {
+	
+	void save(BlogPost blogPost);
+	void update(BlogPost blogPost);
+	void remove(BlogPost blogPost);
+	List<BlogPost> getAllBlogs();
+	BlogPost getBlogById(Long id);
 
 }
---
+++ src/main/java/com/homeninja/dao/impl/BlogTagsDAOImpl.java
@@ -0,0 +1,58 @@
+package com.homeninja.dao.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.homeninja.dao.BlogTagsDAO;
+import com.homeninja.entities.BlogTags;
+
+/**
+ * @author Eugene Ustimenko
+ * @date Oct 15, 2014
+ */
+@Service
+@Transactional
+public class BlogTagsDAOImpl implements BlogTagsDAO {
+
+	@Resource
+	private SessionFactory sessionFactory;
+
+	@Override
+	public void save(BlogTags blogTags) {
+		sessionFactory.getCurrentSession().save(blogTags);
+	}
+
+	@Override
+	public void update(BlogTags blogTags) {
+		sessionFactory.getCurrentSession().update(blogTags);
+	}
+
+	@Override
+	public void remove(BlogTags blogTags) {
+		sessionFactory.getCurrentSession().delete(blogTags);
+	}
+
+	@Override
+	public List<BlogTags> getAllBlogs() {
+		Query query = this.sessionFactory.getCurrentSession().createQuery(
+				"from BlogTags");
+		List<BlogTags> posts = query.list();
+		return posts;
+	}
+
+	@Override
+	public BlogTags getBlogById(Long id) {
+		Query query = this.sessionFactory.getCurrentSession().createQuery(
+				"from BlogTags where id=:id");
+		query.setParameter("id", id);
+		List<BlogTags> list = query.list();
+		return !list.isEmpty() ? list.get(0) : null;
+	}
+
+}
