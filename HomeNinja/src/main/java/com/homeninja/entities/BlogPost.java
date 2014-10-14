9a4238e6b2f1802f3646be624947c3997ea2b631
evgenyustimenko <evgenyustimenko@f0de6d7c-5c98-4a70-9b99-ebcc080b4637>
2014-10-14 17:19:57 +0000

    Hibernate entities

--- HomeNinja/src/main/java/com/homeninja/entities/BlogPost.java
+++ HomeNinja/src/main/java/com/homeninja/entities/BlogPost.java
@@ -1,56 +1,101 @@
 package com.homeninja.entities;
-/*package com.homeninja.entites;
 
-import java.util.List;
+import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "blogs")
 public class BlogPost {
+
+	private Long id;
 	private String title;
 	private String author;
-	private String body;
-	private List<String> tags;
-	private List<String> comments;
-	private String date;
-	
+	private String content;
+	private Integer commentCount;
+	private Date createdDate;
+	private Date modifiedDate;
+	private BlogTags tags;
+
+	@Column(name = "title")
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
+	@Column(name = "author")
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-	public String getBody() {
-		return body;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	public Long getId() {
+		return id;
 	}
-	public void setBody(String body) {
-		this.body = body;
+
+	public void setId(Long id) {
+		this.id = id;
 	}
-	public List<String> getTags() {
-		return tags;
+
+	@Column(name = "content")
+	public String getContent() {
+		return content;
 	}
-	public void setTags(List<String> tags) {
-		this.tags = tags;
+
+	public void setContent(String content) {
+		this.content = content;
 	}
-	public List<String> getComments() {
-		return comments;
+
+	@Column(name = "commentCount")
+	public Integer getCommentCount() {
+		return commentCount;
 	}
-	public void setComments(List<String> comments) {
-		this.comments = comments;
+
+	public void setCommentCount(Integer commentCount) {
+		this.commentCount = commentCount;
 	}
-	public String getDate() {
-		return date;
+
+	@Column(name = "createdDate")
+	public Date getCreatedDate() {
+		return createdDate;
 	}
-	public void setDate(String date) {
-		this.date = date;
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	@Column(name = "modifiedDate")
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
 	}
 	
+	@OneToOne(fetch=FetchType.EAGER, mappedBy="blogPost", cascade=CascadeType.ALL)
+	public BlogTags getTags() {
+		return tags;
+	}
+
+	public void setTags(BlogTags tags) {
+		this.tags = tags;
+	}
 }
-*/
