0ae4527c284a7291c9201eb45ebb7896c4fe85e7
evgenyustimenko <evgenyustimenko@f0de6d7c-5c98-4a70-9b99-ebcc080b4637>
2014-10-16 18:00:40 +0000

    Add blog into database

--- HomeNinja/src/main/java/com/homeninja/controller/BlogPostController.java
+++ HomeNinja/src/main/java/com/homeninja/controller/BlogPostController.java
@@ -1,15 +1,14 @@
 package com.homeninja.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,63 +28,60 @@ import com.homeninja.vo.UserInfo;
 @SessionAttributes("userInfo")
 public class BlogPostController {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(BlogPostController.class);
+    private static final Logger logger = LoggerFactory
+            .getLogger(BlogPostController.class);
 
-	@Resource
-	private BlogPostService blogPostService;
-	
-	@Resource
-	private BlogTagsService blogTagsService;
+    @Resource
+    private BlogPostService blogPostService;
 
-	@RequestMapping(value = "/allBlogs", method = RequestMethod.GET)
-	public String allBlog() {
-		return "allBlogs";
-	}
+    @Resource
+    private BlogTagsService blogTagsService;
 
-	@RequestMapping(value = "/blogDetails", method = RequestMethod.GET)
-	public String blogDetails() {
-		return "blogDetails";
-	}
-	
-	@RequestMapping(value = "/blogPost", method = RequestMethod.GET)
-	public String blogPost() {
-		return "blogPost";
-	}
-	
-	@RequestMapping(value="/postBlog", method = RequestMethod.POST, consumes = "application/json")
-	public String postBlog(HttpServletRequest req, @RequestBody String myObject,Model model){
-		Map modelMap = model.asMap();
-		UserInfo userInfo=null;
-		if(!modelMap.containsKey("userInfo")){
-			return "login";
-		}
-		
-		if(modelMap.containsKey("userInfo")){
-			userInfo = (UserInfo)modelMap.get("userInfo");
-			if(userInfo.getLoggedIn() == null){
-				return "login";
-			}
-			else if(!userInfo.getLoggedIn().equalsIgnoreCase("true")){
-				return "login";
-			}
-		}
-		
-		Gson gson = new Gson();
-		BlogPost post = gson.fromJson(myObject, BlogPost.class);
-		post.setAuthor(userInfo.getUserId());
-		post.setCommentCount(0);
-		Date currentDate = new Date();
-		post.setCreatedDate(currentDate);
-		post.setModifiedDate(currentDate);
-		return "blogPost";
-	}
-	
-	@RequestMapping(value="/tags", method = RequestMethod.GET)
-	public @ResponseBody List<BlogTags> findAllTags(){
-		List<BlogTags> tags = new ArrayList<BlogTags>();
-		tags = blogTagsService.findAllTags();
-		return tags;
-	}
+    @RequestMapping(value = "/allBlogs", method = RequestMethod.GET)
+    public String allBlog() {
+        return "allBlogs";
+    }
+
+    @RequestMapping(value = "/blogDetails", method = RequestMethod.GET)
+    public String blogDetails() {
+        return "blogDetails";
+    }
+
+    @RequestMapping(value = "/blogPost", method = RequestMethod.GET)
+    public String blogPost() {
+        return "blogPost";
+    }
+
+    @RequestMapping(value = "/postBlog", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String postBlog(@RequestBody String myObject, Model model) {
+        Map modelMap = model.asMap();
+        UserInfo userInfo = null;
+        if (!modelMap.containsKey("userInfo")) {
+            return "login";
+        }
+
+        if (modelMap.containsKey("userInfo")) {
+            userInfo = (UserInfo) modelMap.get("userInfo");
+            if (userInfo.getLoggedIn() == null) {
+                return "login";
+            } else if (!userInfo.getLoggedIn().equalsIgnoreCase("true")) {
+                return "login";
+            }
+        }
+
+        Gson gson = new Gson();
+        com.homeninja.gson.bean.BlogPost bean = gson.fromJson(myObject,
+                com.homeninja.gson.bean.BlogPost.class);
+        blogPostService.addBlog(BlogPost.create(bean, userInfo));
+        return "blogPost";
+    }
+
+    @RequestMapping(value = "/tags", method = RequestMethod.GET)
+    public @ResponseBody List<BlogTags> findAllTags() {
+        List<BlogTags> tags = new ArrayList<BlogTags>();
+        tags = blogTagsService.findAllTags();
+        return tags;
+    }
 
 }
