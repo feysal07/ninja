cb1386d2e5af49244144176b68734b9951c09ab7
feysal <feysal@f0de6d7c-5c98-4a70-9b99-ebcc080b4637>
2015-03-20 19:29:33 +0000

    Manage Profile

--- HomeNinja/src/main/java/com/homeninja/vo/City.java
+++ HomeNinja/src/main/java/com/homeninja/vo/City.java
@@ -1,28 +1,38 @@
 package com.homeninja.vo;
 
-public class City {
+public class City implements Comparable<City> {
 
-	private long masterDataId;
-	private String name;
-	private long stateOrderId;
+    private long masterDataId;
+    private String name;
+    private long stateOrderId;
 
-	public long getMasterDataId() {
-		return masterDataId;
-	}
-	public void setMasterDataId(long masterDataId) {
-		this.masterDataId = masterDataId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getStateOrderId() {
-		return stateOrderId;
-	}
-	public void setStateOrderId(long stateOrderId) {
-		this.stateOrderId = stateOrderId;
-	}
-	
+    public long getMasterDataId() {
+        return masterDataId;
+    }
+
+    public void setMasterDataId(long masterDataId) {
+        this.masterDataId = masterDataId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getStateOrderId() {
+        return stateOrderId;
+    }
+
+    public void setStateOrderId(long stateOrderId) {
+        this.stateOrderId = stateOrderId;
+    }
+
+    @Override
+    public int compareTo(City o) {
+        return this.name.compareTo(o.getName());
+    }
+
 }
